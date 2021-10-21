e256df0c8d0af8d8ef4368dc5ba09df9e61c5d2d
Wilker Lucio <wilkerlucio@gmail.com>
2021-10-21 18:39:15 -0300

    Call though HTTP

--- src/main/com/wsscode/presentations/pathom3_data_nav/micro_services/federator.clj
+++ src/main/com/wsscode/presentations/pathom3_data_nav/micro_services/federator.clj
@@ -7,36 +7,40 @@
             [com.wsscode.presentations.pathom3-data-nav.micro-services.shipping :as mr.shipping]
             [com.wsscode.pathom3.connect.foreign :as pcf]
             [com.wsscode.pathom3.interface.eql :as p.eql]
-            [com.wsscode.pathom3.interface.async.eql :as p.a.eql]))
+            [com.wsscode.pathom3.interface.async.eql :as p.a.eql]
+            [com.wsscode.presentations.pathom3-data-nav.pathom-server :as ps]))
+
+(defn start-all []
+  (ps/start-server mr.customer/env {::ps/port 3011})
+  (ps/start-server mr.line-items/env {::ps/port 3012})
+  (ps/start-server mr.order/env {::ps/port 3013})
+  (ps/start-server mr.product/env {::ps/port 3014})
+  (ps/start-server mr.shipping/env {::ps/port 3015}))
+
+(defonce initial-start (start-all))
+
+(defonce plan-cache* (atom {}))
 
 (def env
-  (-> {::p.a.eql/parallel? true}
+  (-> {:com.wsscode.pathom3.connect.planner/plan-cache* plan-cache*}
       (pci/register
-        [(pcf/foreign-register mr.customer/request)
-         (pcf/foreign-register mr.line-items/request)
-         (pcf/foreign-register mr.order/request)
-         (pcf/foreign-register mr.product/request)
-         (pcf/foreign-register mr.shipping/request)])
+        [ ;(pcf/foreign-register mr.customer/request)
+         ;(pcf/foreign-register mr.line-items/request)
+         ;(pcf/foreign-register mr.order/request)
+         ;(pcf/foreign-register mr.product/request)
+         ;(pcf/foreign-register mr.shipping/request)
+
+         (pcf/foreign-register (ps/server-handler 3011))
+         (pcf/foreign-register (ps/server-handler 3012))
+         (pcf/foreign-register (ps/server-handler 3013))
+         (pcf/foreign-register (ps/server-handler 3014))
+         (pcf/foreign-register (ps/server-handler 3015))])
       ((requiring-resolve 'com.wsscode.pathom.viz.ws-connector.pathom3/connect-env)
        "debug")))
 
 (comment
 
-  @(p.a.eql/process env
-    {:acme.order/id 1628545763873}
-    [:acme.order/id
-     :acme.user/first-name
-     :acme.order/delivery-state
-     {:acme.order/line-items
-      [:acme.line-item/quantity
-       :acme.product/title
-       :acme.line-item/price-total]}
-     :acme.order/delivery-fee
-     :acme.order/discount
-     :acme.order/items-total
-     :acme.order/grand-total
-     :acme.user/cpf
-     :acme.user/phone])
+  {:acme.order/id {}}
 
   (p.eql/process env
     {:acme.order/id 1628545763873}
