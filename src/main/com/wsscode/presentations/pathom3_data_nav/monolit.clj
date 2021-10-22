c201f82de220784294a483f15e01a59af4a59bf3
Wilker Lucio <wilkerlucio@gmail.com>
2021-10-22 00:25:15 -0300

    Monolit

--- src/main/com/wsscode/presentations/pathom3_data_nav/monolit.clj
+++ src/main/com/wsscode/presentations/pathom3_data_nav/monolit.clj
@@ -1,1 +1,56 @@
-(ns com.wsscode.presentations.pathom3-data-nav.monolit)
+(ns com.wsscode.presentations.pathom3-data-nav.monolit
+  (:require [com.wsscode.pathom3.connect.indexes :as pci]
+            [com.wsscode.presentations.pathom3-data-nav.micro-services.customer :as mr.customer]
+            [com.wsscode.presentations.pathom3-data-nav.micro-services.line-items :as mr.line-items]
+            [com.wsscode.presentations.pathom3-data-nav.micro-services.order :as mr.order]
+            [com.wsscode.presentations.pathom3-data-nav.micro-services.product :as mr.product]
+            [com.wsscode.presentations.pathom3-data-nav.micro-services.shipping :as mr.shipping]
+            [com.wsscode.pathom3.interface.eql :as p.eql]
+            [com.wsscode.presentations.pathom3-data-nav.pathom-server :as ps]))
+
+(defonce plan-cache* (atom {}))
+
+(def env
+  (-> {:com.wsscode.pathom3.connect.planner/plan-cache* plan-cache*}
+      (pci/register
+        [mr.customer/registry
+         mr.line-items/registry
+         mr.order/registry
+         mr.product/registry
+         mr.shipping/registry])
+      ((requiring-resolve 'com.wsscode.pathom.viz.ws-connector.pathom3/connect-env)
+       "debug")))
+
+(comment
+  (p.eql/process env
+    {:acme.order/id 1628545763873}
+    [:acme.order/id
+     :acme.user/first-name
+     :acme.order/delivery-state
+     {:acme.order/line-items
+      [:acme.line-item/quantity
+       :acme.product/title
+       :acme.line-item/price-total]}
+     :acme.order/delivery-fee
+     :acme.order/discount
+     :acme.order/items-total
+     :acme.order/grand-total
+     :acme.user/cpf
+     :acme.user/phone])
+
+  (p.eql/process env
+    {:acme.order/id 1628545763873}
+    [{:acme.order/line-items
+      [:acme.product/title]}
+
+     :acme.user/cpf
+     :acme.user/phone])
+
+  (p.eql/process env
+    {:records
+     [{:amount 1000}
+      {:precise-amount 23.4M}
+      {:precise-amount 25.4M}
+      {:amount 400}]}
+    [{:records
+      [:precise-amount]}]))
